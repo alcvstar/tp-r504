40359d19ef437791a413c37b9c1b0565d51a6891
alcvstar <alcvstar@gmail.com>
2024-03-21 13:15:03 +0100

    update script sh

--- TP14/script.sh
+++ TP14/script.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-sudo apt update -y && sudo apt upgrade -y
-sudo debsecan --suite bullseye | wc -l && sudo debsecan --suite bullseye --only-fixed | wc -l
+
+LOG_FILE="/var/log/debsecan.log"
+
+debsecan --update >> "$LOG_FILE" 2>&1
+debsecan --quiet --output-format=pretty >> "$LOG_FILE" 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "$(date): La recherche de vulnérabilités avec debsecan a été exécutée avec succès." >> "$LOG_FILE"
+else
+    echo "$(date): Erreur lors de l'exécution de la recherche de vulnérabilités avec debsecan." >> "$LOG_FILE"
+fi
+
+echo "$(date): Script debsecan terminé." >> "$LOG_FILE"
